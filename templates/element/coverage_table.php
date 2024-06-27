734d8610e6de7b81878052546890a55662e52eda
mscherer <dereuromark@web.de>
2024-06-27 16:00:11 +0200

    Upgrading finish

--- templates/element/coverage_table.php
+++ templates/element/coverage_table.php
@@ -8,15 +8,15 @@
 <table class="table table-striped">
 <tr><th><?php echo __d('translate', 'Language'); ?></th><th><?php echo __d('translate', 'Coverage'); ?></th><th><?php echo __d('translate', 'Active'); ?></th></tr>
 <?php foreach ($languages as $language) {
-if (!isset($coverage[$language['iso2']])) {
+if (!isset($coverage[$language->locale])) {
 	$currentCoverage = 0.0;
 } else {
-	$currentCoverage = $coverage[$language['iso2']];
+	$currentCoverage = $coverage[$language->locale];
 }
 $currentColor = $this->Translation->getColor($currentCoverage);
 ?>
 <tr>
-	<td><?php echo $this->Translation->flag($language['code']); ?> <?php echo h($language['name']); ?></td>
+	<td><?php echo $this->Translation->flag($language['code']); ?> <?php echo h($language['name']); ?> (<?php echo h($language->locale); ?>)</td>
 	<td><span style="color:#<?php echo $currentColor;?>;font-weight:bold"><?php echo $currentCoverage; ?>%</span></td>
 	<td><?= $this->element('Translate.yes_no', ['value' => $language['active']]) ?></td>
 </tr>
